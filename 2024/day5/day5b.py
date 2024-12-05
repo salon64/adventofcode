2187b9a80c3681ba835e30376842280a7f45e1aa
malco <56635871+salon64@users.noreply.github.com>
2024-12-05 20:02:09 +0100

    day5

--- 2024/day5/day5b.py
+++ 2024/day5/day5b.py
@@ -1,4 +1,4 @@
-with open('2024/day5/test', 'r') as file: # 2024/day5/day_5_input.txt
+with open('2024/day5/day_5_input.txt', 'r') as file: # 2024/day5/day_5_input.txt
 	data = file.read().splitlines()
 
 rules = []
@@ -7,48 +7,73 @@ for i, item in enumerate(data):
 	if item == '':
 		rules = data[:i]
 		updates = data[i+1:]
-		break
 
 rules = [instr.split('|') for instr in rules]
 for i, rule in enumerate(rules):
 	rules[i] = [int(rule[0]), int(rule[-1])]
 
 rules_d = {}
+custom_rules = {}
 for first, second in rules:
     if first not in rules_d:
         rules_d[first] = []
     rules_d[first].append(second)
 
+
+
 updates = [[int(item) for item in update.split(',')] for update in updates]
 print(rules_d)
 
+def lawAbidingNumber(num1, num2):
+	return num1 in rules_d and num2 in rules_d[num1]
+
+def sortingTheNumbersInAWayThatWillNotCauseMeShame(update):
+	sortedList = []
+	for item in update:
+		pos = len(update)-1
+		for i, sortedItem in enumerate(sortedList):
+			if lawAbidingNumber(sortedItem, item):
+				pos = i
+				break
+		sortedList.insert(pos, item)
+	return sortedList
+
+
+# def fix(update):
+# 	for j, item in enumerate(update):
+# 		if item in rules_d:
+# 			for val in rules_d[item]:
+# 				if val in update[:j]:
+# 					swapPos = 0
+# 					for s, ite in enumerate(update):
+# 						if val == ite:
+# 							swapPos = s
+
+# 					update[j], update[swapPos] = update[swapPos], update[j]
+# 					# print(update)
+# 					fix(update)
+	
+# 	return(update)
+		
+
 result = 0
 for i, update in enumerate(updates):
-	print(update)
 	okay = True
 	for j, item in enumerate(update):
 		if item in rules_d:
 			for val in rules_d[item]:
 				while val in update[:j]:
 					okay = False
-					
-					swapPos = 0
-					for s, ite in enumerate(update):
-						if val == ite:
-							swapPos = s
-							break
-					
-					update[j], update[swapPos] = update[swapPos], update[j]
-					print("-----------------------------------------------------")
-					print(f"false {val} in {update[:i]},   key: {item} values: {rules_d[item]}")
-					print(update)
-					print("--------------------------------------------------------")
+					break
 
 
 
 	if not okay:
-		# print(f"---------------- {update[int(len(update)/2)]}")
+		update = sortingTheNumbersInAWayThatWillNotCauseMeShame(update)
 		result += update[int(len(update)/2)]
 
+
 print(result)
+# 4971
+
 
