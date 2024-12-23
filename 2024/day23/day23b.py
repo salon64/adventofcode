ac336884667013343942483163d87edc90dea127
malco <56635871+salon64@users.noreply.github.com>
2024-12-23 22:00:37 +0100

    day23

--- 2024/day23/day23b.py
+++ 2024/day23/day23b.py
@@ -1,73 +1,18 @@
-from collections import defaultdict
-import copy
-from itertools import permutations
-import itertools
-import re
 import time
-import heapq
-import bisect
+import networkx as nx
 start_time = time.time()
 with open('2024/day23/day_23_input.txt', 'r') as file: # day_23_input.txt
     data = file.read().strip().splitlines()
 
-connections = [line.split('-') for line in data]
-computer_connections = defaultdict(set)
-# print(connections)
-                  
-for comp1, comp2 in connections:
-    if comp1 != comp2:
-        computer_connections[comp1].add(comp2)
-        computer_connections[comp2].add(comp1)
-# print(computer_connections) 
+G = nx.Graph()
+for line in data:
+    l, r = line.split('-')
+    G.add_edge(l, r)
 
+largest_clique = []
+for clique in nx.enumerate_all_cliques(G):
+    largest_clique = clique  # overwrite with the latest (largest) clique
+print(",".join(sorted(largest_clique)))
 
-biggest_network = 0
-# cache = {}
-def next_connection(curr_comp, path):
-    global biggest_network
-    all_visited = True
-    for connected_comp in computer_connections[curr_comp]:
-        if connected_comp not in path:  # Avoid revisiting nodes
-            bisect.insort(path, connected_comp) # insert, sorted
-            next_connection(connected_comp, path[:])
-            all_visited = False
-    
-    if all_visited:
-        biggest_network = max(biggest_network, len(path))
-
-
-
-computers = set()
-for comps in connections:
-    for c in comps:
-        computers.add(c)
-# print(computers)
-for i, comp in enumerate(computers):
-    # print(f"{comp},  {[comp]}")
-    print(f"{i} of {len(computers)}")
-    next_connection(comp, [comp])
-
-
-
-print(f"biggest network: {biggest_network}")
 end_time = time.time()
 print(f'Time took: {round((end_time - start_time) * 1000, 2)}ms')
-
-
-# for i, tri in enumerate(tripplets):
-#     a, b, c = tri
-#     # print(tripplets[i], end="")
-#     okay = False
-#     if a[0] == 't' or a[-1] == 't':
-#         okay = True
-#     elif b[0] == 't' or b[-1] == 't':
-#         okay = True
-#     elif c[0] == 't' or c[-1] == 't':
-#         okay = True
-
-#     if okay:
-#         tripplets_with_t += 1
-#         # print(" okay")
-#     else:
-#         # print(" not okay")
-#         pass
